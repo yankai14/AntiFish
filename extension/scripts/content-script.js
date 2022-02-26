f95472cf983b34de87c934ef8433882fce3d829a
her <58970041+limherhuey@users.noreply.github.com>
2022-02-27 02:47:23 +0800

    Push post

--- extension/scripts/content-script.js
+++ extension/scripts/content-script.js
@@ -1,3 +1,7 @@
+const coreApi = axios.create({
+    baseURL: 'https://asia-southeast1-spry-sentry-340405.cloudfunctions.net',
+});
+  
 
 document.addEventListener('click', function (e) {
     if (e.target.tagName == "A" || e.target.tagName == "a") {
@@ -8,21 +12,47 @@ document.addEventListener('click', function (e) {
         var exp = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
         var regex = new RegExp(exp)
 
+        // const response = await coreApi.get("https://asia-southeast1-spry-sentry-340405.cloudfunctions.net")
+        // alert(response.data)
+
+        // call api here if unsafe then inject
+        // var xhr = new XMLHttpRequest();
+        // xhr.open("GET", "https://asia-southeast1-spry-sentry-340405.cloudfunctions.net/validateUrl", true);
+        // xhr.onreadystatechange = function() {
+        //     if (xhr.readyState == 4) {
+        //         // JSON.parse does not evaluate the attacker's scripts.
+        //         var resp = JSON.parse(xhr.responseText);
+        //         alert(resp)
+        //     }
+        // }
+        // xhr.onload = function() {
+        //     alert("sdhifuish")
+        // }
+        // xhr.send({
+        //     "url": "https://www.goldmansachs.com.sg/"
+        // });
+
+        // encodeURIComponent(flskdj)
+
+        $.ajax({
+            type: 'GET',
+            url: 'https://asia-southeast1-spry-sentry-340405.cloudfunctions.net/validateUrl?url=https%3A%2F%2Fwww.goldmansachs.com.sg%2F',
+            // dataType: 'json',
+            // crossDomain: true,
+            success: function(responseData, textStatus, jqXHR) {
+                var value = responseData.fish;
+                alert(value)
+            },
+            error: function (responseData, textStatus, errorThrown) {
+                alert('GET failed.');
+            }
+        });
+        
         if (regex.test(text)) {
             if (url == text || url == text + "/") {
                 console.log("valid")
             } else {
-                // alert("Possible phishing detected!")
-
-                var unsafeBadge = document.createElement("div")
-                var unsafeText = document.createElement("p")
-                unsafeText.textContent = "Unsafe!"
-                unsafeText.style.cssText = "color: #ffffff; font-size: 32%; font-weight: 600; font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;"
-                unsafeBadge.style.cssText = "text-align: center; display: inline-block; vertical-align: baseline; white-space: nowrap; line-height: 0.001; padding-left: 5px; padding-right: 5px; border-radius: 6px; margin-left: 5px; background: #dc3545;"
-                unsafeBadge.appendChild(unsafeText)
-
-                parent.append(unsafeBadge)
-
+                injectUnsafeBadge(parent, e.target.nextSibling)
                 console.log("Phishing detected!")
                 e.preventDefault()
             }
@@ -34,3 +64,17 @@ document.addEventListener('click', function (e) {
     }
     // e.preventDefault();
 }, true);
+
+function injectUnsafeBadge(parent, nextSibling) {
+    if (!parent.querySelector("#badge")) {
+        var unsafeBadge = document.createElement("div")
+        var unsafeText = document.createElement("p")
+        unsafeText.textContent = "Unsafe!"
+        unsafeText.style.cssText = "color: #ffffff; font-size: 32%; font-weight: 600; font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;"
+        unsafeBadge.style.cssText = "text-align: center; display: inline-block; vertical-align: baseline; white-space: nowrap; line-height: 0.001; padding-left: 5px; padding-right: 5px; border-radius: 6px; margin-left: 5px; margin-right: 5px; background: #dc3545;"
+        unsafeBadge.setAttribute("id", "badge")
+        unsafeBadge.appendChild(unsafeText)
+
+        parent.insertBefore(unsafeBadge, nextSibling)
+    }
+}
