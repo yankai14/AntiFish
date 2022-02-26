731ba9f5d7c296eaf78a5c9a67bd2922d9db0412
her <58970041+limherhuey@users.noreply.github.com>
2022-02-26 21:50:35 +0800

    added html injection of 'unsafe' badge

--- extension/scripts/content-script.js
+++ extension/scripts/content-script.js
@@ -1,43 +1,36 @@
-$(document).ready(() => {
-    var head = document.head;
-    var link = document.createElement("link");
-    link.type = "text/css";
-    link.rel = "stylesheet";
-    link.href = "../ui/bootstrap.css";
-    head.appendChild(link);
-});
 
-
-document.addEventListener('click', function(e) {
-    if (e.target.tagName == "A" || e.target.tagName == "a"){
+document.addEventListener('click', function (e) {
+    if (e.target.tagName == "A" || e.target.tagName == "a") {
         var url = e.target.href
         var text = e.target.text
         var parent = e.target.parentNode
 
-        console.log(parent)
-        
-        var unsafeBadge = document.createElement("span")
-        unsafeBadge.classList.add("badge", "badge-danger")
-        unsafeBadge.textContent = "Unsafe!"
-
-        
-        parent.append(unsafeBadge)
-        
         var exp = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
         var regex = new RegExp(exp)
 
         if (regex.test(text)) {
-            if (url == text || url == text + "/"){
+            if (url == text || url == text + "/") {
                 console.log("valid")
             } else {
-                alert("Possible phishing detected!")
+                // alert("Possible phishing detected!")
+
+                var unsafeBadge = document.createElement("div")
+                var unsafeText = document.createElement("p")
+                unsafeText.textContent = "Unsafe!"
+                unsafeText.style.cssText = "color: #ffffff; font-size: 32%; font-weight: 600; font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;"
+                unsafeBadge.style.cssText = "text-align: center; display: inline-block; vertical-align: baseline; white-space: nowrap; line-height: 0.001; padding-left: 5px; padding-right: 5px; border-radius: 6px; margin-left: 5px; background: #dc3545;"
+                unsafeBadge.appendChild(unsafeText)
+
+                parent.append(unsafeBadge)
+
                 console.log("Phishing detected!")
                 e.preventDefault()
             }
         } else {
             console.log("No match");
         }
+
+        e.stopPropagation();
     }
-    e.stopPropagation();
     // e.preventDefault();
 }, true);
