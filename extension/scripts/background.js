31523e497073c6f1953c8bdcd1f0b6e9c8d7c096
Chen Yifan <chen.yiafn@gmail.com>
2022-02-26 18:09:44 +0800

    Initial functionality

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,6 +1,7 @@
 let color = '#FFFFFF';
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cwhite', `color: ${color}`);
+//   chrome.storage.sync.set({ color });
+//   console.log('Default background color set to %cwhite', `color: ${color}`);
+
 });
---
+++ extension/scripts/content-script.js
@@ -0,0 +1,25 @@
+document.addEventListener('click', function(e) {
+    if (e.target.tagName == "A" || e.target.tagName == "a"){
+        var url = e.target.href;
+        var text = e.target.text;
+        
+        var exp = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+        var regex = new RegExp(exp);
+
+
+
+        if (regex.test(text)) {
+            if (url == text || url == text + "/"){
+                console.log("valid")
+            }else{
+                alert("Possible phishing detected!")
+                console.log("Phishing detected!")
+                e.preventDefault()
+            }
+        } else {
+            console.log("No match")
+        }
+    }
+    e.stopPropagation();
+    // e.preventDefault();
+}, true);
