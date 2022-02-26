227e5e43d9b8b9c14e973b8fda8163311c18d676
Chen Yifan <chen.yiafn@gmail.com>
2022-02-26 14:57:53 +0800

    Initial

---
+++ extension/scripts/background.js
@@ -0,0 +1,6 @@
+let color = '#FFFFFF';
+
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cwhite', `color: ${color}`);
+});
